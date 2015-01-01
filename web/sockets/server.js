c0838080aa28073435e900b21d7b97396ced3b97
Locke <moyerock@gmail.com>
2015-01-01 11:29:23 +0800

    add event handler on server-side.

--- web/sockets/server.js
+++ web/sockets/server.js
@@ -9,14 +9,14 @@ module.exports.createServer = createServer;
 function createServer(app){
     var server = http.Server(app.callback());
     io = io(server);
-    eventHandler(io);
+    messageHandler(io);
     return server;
 }
 
 /*
  * socket event handler
  */
-function eventHandler(io) {
+function messageHandler(io) {
     io.on('connection', function (socket) {
         socket.emit('news', {greeting: 'welcome.'});
         socket.on('broadcast', function (data) {
