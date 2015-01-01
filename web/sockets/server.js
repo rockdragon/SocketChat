b332713240187c2136b41efeac29ab0349807423
Locke <moyerock@gmail.com>
2015-01-01 11:14:15 +0800

    add event handler on server-side.

--- web/sockets/server.js
+++ web/sockets/server.js
@@ -1,12 +1,26 @@
 var io = require('socket.io');
 var http = require('http');
 
+module.exports.createServer = createServer;
+
 /*
 * @app: Koa application
 * */
 function createServer(app){
     var server = http.Server(app.callback());
     io = io(server);
+    eventHandler(io);
     return server;
 }
-module.exports.createServer = createServer;
+
+/*
+ * socket event handler
+ */
+function eventHandler(io) {
+    io.on('connection', function (socket) {
+        socket.emit('news', {greeting: 'welcome.'});
+        socket.on('broadcast', function (data) {
+            console.log(data);
+        });
+    });
+}
